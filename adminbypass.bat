8ff29f5872e56ecc4e92127743af5fa61f3600d4
Windows10-AdminHack <76558886+Windows10-AdminHack@users.noreply.github.com>
2020-12-24 15:50:58 +0000

    Update adminbypass.bat

--- adminbypass.bat
+++ adminbypass.bat
@@ -1,1 +1,3 @@
-cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" "%1""
+@echo on
+REM don`t use this file, it is outdated!
+REM cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" "%1""
